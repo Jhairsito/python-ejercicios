f147abf2b46010ae8c6168f884d5ffd2447955ef
jhair <fjhair35@gmail.com>
2023-08-23 11:40:29 -0500

    python

---
+++ secuenciales/01s.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls")
+
+varones = int(input("ingrese la cantidad de varones : "))
+mujeres = int(input("ingrese la cantidad de mujeres : "))
+
+total = varones + mujeres 
+Pm = (mujeres / total)*100
+Pv = (varones / total)*100
+
+print ("porsentaje de varones en el aula : "{:.2f}%".format("Pv")")
+print ("porsentaje de mujeres en el aula : "{:.2f}%".format("Pm")")
