cbe6a52a93d252b0c5acd9b540f2bf19f3cc00f9
jhair <fjhair35@gmail.com>
2023-08-23 17:14:02 -0500

    pythonn

---
+++ secuenciales/12s.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+a = int(input("ingrese valor de a : "))
+b = int(input("ingrese valor de b : "))
+c = int(input("ingrese valor de c : "))
+x = int(input("ingrese valor de x : "))
+
+desarrollo = (a * (x * x)) + ((b * x) + c)
+
+print ("el resultado es:",desarrollo)
