6f8a0e65cb15296d6cb6f1cc280e2e3327bce1a5
jhair <fjhair35@gmail.com>
2023-08-23 15:14:38 -0500

    python

---
+++ secuenciales/07s.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+b = int(input("Ingrese la base del rectangulo :"))
+h = int(input("ingres la altura del rectangulo :"))
+
+area = b * h
+perimetro = 2 * (b * h)
+
+print ("el area del rectangulo es :",area)
+print ("el perimetro del rectangulo es :",perimetro)
